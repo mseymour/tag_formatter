f0644a2363642b651e1f1525a616793c7ed2e439
Mark Seymour <mark.seymour.ns@gmail.com>
2012-01-21 17:20:47 -0400

    Adding formatter class

---
+++ lib/tag_formatter/formatter.rb
@@ -0,0 +1,69 @@
+# coding: utf-8
+
+module TagFormatter
+  class Formatter
+    @@_known_params = [:tags, :tag_start, :tag_end, :inline_comment_delimiter, :block_comment_start, :block_comment_end]
+    attr_accessor :input
+    attr_accessor *@@_known_params
+
+    # @param [String, File] input The input; may be either a String or a File.
+    # @param [Hash] params The options for parsing the String or File.
+    # @option params [Hash] :tags ({}) Key+value pairs representing the tags and values associated with the tags.
+    # @option params [String] :tag_start ({) The character(s) denoting the beginning of a tag.
+    # @option params [String] :tag_end (}) The character(s) denoting the end of a tag.
+    # @option params [String] :inline_comment_delimiter (#) The character(s) denoting an inline comment
+    # @option params [String] :block_comment_start (/*) The character(s) denoting the beginning of a comment block.
+    # @option params [String] :block_comment_end (*/) The character(s) denoting the end of a comment block.
+    # @raise [TypeError] #input must be a String or File.
+    def initialize input, params={}
+      params = {
+        tags: {},
+        tag_start: '{',
+        tag_end: '}',
+        inline_comment_delimiter: '#',
+        block_comment_start: '/*',
+        block_comment_end: '*/'
+      }.merge(params)
+      
+      # Assigning input to @input if string
+      @input = if input.is_a? String
+        input
+      # Reading input and assigning the resultant string to @input
+      elsif input.is_a? File
+        input.read.to_s
+      # Raise a TypeError.
+      else
+        raise TypeError, "input was an `#{input.class.name}', expected a `String' or `File'."
+      end
+      # Assign the param values to the (known) attributes.
+      @@_known_params.each do |attr|
+        self.send(attr.to_s + "=", params[attr])
+      end
+    end
+
+    def parse!
+      tagify(decommentify(@input)).strip
+    end
+
+    private
+
+    # @param [String] input The string to decommentify.
+    def decommentify input
+      output = input.dup
+      # Remove multiline comments:
+      output.gsub!(/(#{Regexp.quote @block_comment_start}.+?#{Regexp.quote @block_comment_end})/m, "")
+      # Remove inline comments:
+      output.gsub!(/(#{Regexp.quote @inline_comment_delimiter}.+$)/,"")
+      return output.lines.map(&:strip).join($/)
+    end
+
+    # @param [String] input The string to tagify.
+    # @raise [StandardError] @tags must not be empty.
+    def tagify input
+      output = input.dup
+      raise StandardError, "@tags is empty!" if @tags.empty? #improve on this
+      @tags.each {|key,value| output.gsub!(tag_start.to_s+key.to_s+tag_end.to_s, value.to_s)}
+      return output
+    end
+  end
+end
